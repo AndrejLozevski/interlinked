cd41e12a22ac45ecc6aa28457820392818bd1092
Andrej Lozevski <andrejlozevski@gmail.com>
2026-03-31 16:47:53 -0400

    added scripts

---
+++ interlinked/__init__.py
@@ -0,0 +1,3 @@
+from . import interlinked
+
+add = interlinked.add
---
+++ scripts/example.py
@@ -0,0 +1,9 @@
+import interlinked as nex
+def main():
+    x = 10
+    y = 20
+    result = nex.add(x, y)
+    print(f'{x} + {y} = {result}')
+
+if __name__ == '__main__':
+    main()
