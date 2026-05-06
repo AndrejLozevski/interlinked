f830e0b7900e98dde6df39f535dd809bad3ae642
Andrej Lozevski <andrejlozevski@gmail.com>
2026-05-06 18:44:21 -0400

    rebuilt again

--- interlinked/__init__.py
+++ interlinked/__init__.py
@@ -1,8 +1,6 @@
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] - %(message)s")
 
-
-
 from .       import meta
 from .config import config
 from .       import utils
