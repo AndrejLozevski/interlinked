6d73851ee17f9ec734df44544961d044642bfa08
Andrej Lozevski <andrejlozevski@gmail.com>
2026-05-11 18:32:40 -0400

    added meta.success()

--- interlinked/meta.py
+++ interlinked/meta.py
@@ -4,8 +4,17 @@ import logging
 log = logging.getLogger(__name__)
 
 
+MSG_SUCCESS   = "Script completed!"
+MSG_ERROR     = "Error occured!"
+MSG_EXCEPTION = "Exception occured!"
+
+class Success(SystemExit):
+    def __init__(self, message=MSG_SUCCESS, *args):
+        log.info(message, *args)
+        super().__init__(0)
+
 class Error(SystemExit):
-    def __init__(self, message, *args, error=None):
+    def __init__(self, message=MSG_ERROR, *args, error=None):
         if error is not None:
             name = error.__name__ if isinstance(error, type) else str(error)
             message = f"{name}: {message}"
@@ -13,7 +22,7 @@ class Error(SystemExit):
         super().__init__(1)
 
 class Exception(Exception):
-    def __init__(self, message, *args, exception=None):
+    def __init__(self, message=MSG_EXCEPTION, *args, exception=None):
         if exception is not None:
             name = exception.__name__ if isinstance(exception, type) else str(exception)
             message = f"{name}: {message}"
