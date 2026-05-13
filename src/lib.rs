a06b984f0f4b5f2b3983fbdedcd223d07c8fa765
Andrej Lozevski <andrejlozevski@gmail.com>
2026-05-12 23:09:12 -0400

    optimized rayon multithreading in rust fxns

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use pyo3::prelude::*;
+use rayon::ThreadPoolBuilder;
 mod stats;
 mod info;
 
 #[pymodule]
 fn interlinked(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    ThreadPoolBuilder::new()
+        .num_threads(4)
+        .build_global()
+        .unwrap();
+
     let stats_mod = PyModule::new(py, "stats")?;
     stats_mod.add_function(wrap_pyfunction!(stats::pearson_corr,  &stats_mod)?)?;
     stats_mod.add_function(wrap_pyfunction!(stats::spearman_corr, &stats_mod)?)?;
